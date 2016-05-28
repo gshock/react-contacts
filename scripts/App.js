0f108f62382de543333b44223f00a8f923ab24a8
Gerardo A <zdoggie@gmail.com>
2016-05-28 15:32:12 -0400

    filtered contacts

--- scripts/App.js
+++ scripts/App.js
@@ -12,8 +12,9 @@ class ContactsApp extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <ContactList contacts={this.props.contacts} />
+        <SearchBar filterText={this.state.filterText} />
+        <ContactList contacts={this.props.contacts}
+          filterText={this.state.filterText}/>
       </div>
     )
   }
@@ -25,16 +26,23 @@ ContactsApp.propTypes = {
 //search bar
 class SearchBar extends Component {
   render() {
-    return <input type="search" placeholder="search" />
+    return <input type="search" placeholder="search"
+      value={this.props.filterText} />
   }
+}
+SearchBar.propTypes = {
+  filterText: PropTypes.string.isRequired
 }
 
 //contacts list
 class ContactList extends Component {
   render() {
+    let filteredContacts = this.props.contacts.filter(
+      (contact) => contact.name.indexOf(this.props.filterText) !== -1
+    );
     return (
       <ul>
-        {this.props.contacts.map(
+        {filteredContacts.map(
           (contact) => <ContactItem key={contact.email}
             name={contact.name}
             email={contact.email} />

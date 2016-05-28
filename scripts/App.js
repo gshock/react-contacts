cb56e66cb7008a5bbae8894d2a5338241ebc7143
Gerardo A <zdoggie@gmail.com>
2016-05-28 15:31:32 -0400

    checked in working

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
 
-export default class App extends Component {
+// Main component. Renders a SearchBar and a ContactList
+class ContactsApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterText: ''
+    };
+  }
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
-    );
+      <div>
+        <SearchBar />
+        <ContactList contacts={this.props.contacts} />
+      </div>
+    )
   }
 }
+ContactsApp.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object)
+}
+
+//search bar
+class SearchBar extends Component {
+  render() {
+    return <input type="search" placeholder="search" />
+  }
+}
+
+//contacts list
+class ContactList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.contacts.map(
+          (contact) => <ContactItem key={contact.email}
+            name={contact.name}
+            email={contact.email} />
+        ) }
+      </ul>
+    )
+  }
+}
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object)
+}
+
+//contact item
+class ContactItem extends Component {
+  render() {
+    return <li>{this.props.name} - {this.props.email}</li>
+  }
+}
+ContactItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+}
+
+//contacts data
+let contacts = [
+  { name: "Cassio Zen", email: "cassiozen@gmail.com" },
+  { name: "Dan Abramov", email: "gaearon@somewhere.com" },
+  { name: "Pete Hunt", email: "floydophone@somewhere.com" },
+  { name: "Paul O’Shannessy", email: "zpao@somewhere.com" },
+  { name: "Ryan Florence", email: "rpflorence@somewhere.com" },
+  { name: "Sebastian Markbage", email: "sebmarkbage@here.com" },
+]
+
+render(<ContactsApp contacts={contacts} />, document.getElementById('root'));
